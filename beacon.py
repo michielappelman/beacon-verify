b31bc7d7196d0c43f25ce3718476d31cba2f792b
Michiel Appelman <michiel@appelman.se>
2015-01-04 00:59:43 +0100

    Last one was just a move, this one actually makes the script more modular and moved to argparse.

--- beacon.py
+++ beacon.py
@@ -6,83 +6,94 @@ import time
 import OpenSSL
 import hashlib
 import requests
+import argparse
 import xml.etree.ElementTree as ET
 from optparse import OptionParser
 
-record_url = 'https://beacon.nist.gov/rest/record/'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('timestamp', nargs='?', default="last", 
+                      help=argparse.SUPPRESS)
+    parser.add_argument("-c", "--cert", dest="cfile", metavar="CERT",
+                      help="read certificate from CERT", default='./beacon.cer')
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true",
+                      help="be more verbose, otherwise only errors are printed")
+    args = parser.parse_args()
 
-usage = "Usage: %prog [options] [timestamp]"
-parser = OptionParser(usage)
-parser.add_option("-c", "--cert", dest="cfile", metavar="CERT",
-                  help="read certificate from CERT", default='./beacon.cer')
-parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
-                  help="be more verbose, otherwise only errors are printed")
-(options, args) = parser.parse_args()
+    beacon = get(args.timestamp)
 
-if len(args) > 1:
-    parser.error("Too many arguments, taking only one timestamp!")
-    sys.exit(1)
-if len(args) == 0:
-    if options.verbose:
-        print "No timestamp provided, checking latest beacon."
-    ts = "last"
-if len(args) == 1:
+    if args.verbose:
+        print "Timestamp: " + beacon['timeStamp']
+        print "Output Value: " + beacon['outputValue']
+
+    if verify(beacon['signatureValue'], beacon['data'], args.cfile) and args.verbose:
+        print "Beacon signature verification passed."
+
+    if check_output(beacon['signatureValue'], beacon['outputValue']) and args.verbose:
+        print "Signature hash successfully matches Output Value."
+
+def get(timestamp='last', base_url='https://beacon.nist.gov/rest/record/'):
+    """Load a certain beacon record.
+
+    timestamp -- integer or string "last" (default: "last")
+    base_url  -- base URL for beacon record XML
+                    (default: https://beacon.nist.gov/rest/record/)"""
+    if timestamp != "last":
+        try:
+            int(timestamp)+1
+        except ValueError:
+            print "Provided timestamp must be integer or 'last'"
+            sys.exit(1)
+    record = requests.get(base_url + timestamp, verify=True)
+    xml = ET.fromstring(record.content)
+    beacon = {}
+    for child in xml:
+        beacon[child.tag] = child.text
+    # Add the data to be signed to the dict as well.
+    beacon['data'] = beacon['version'] + \
+        "{0:08x}".format(int(beacon['frequency'])).decode("hex") + \
+        "{0:016x}".format(int(beacon['timeStamp'])).decode("hex") + \
+        beacon['seedValue'].decode("hex") + \
+        beacon['previousOutputValue'].decode("hex") + \
+        "{0:08x}".format(int(beacon['statusCode'])).decode("hex")
+    return beacon
+
+def reverse_sign(signature):
+    """Converts a beacon signature from Little-Endian to Big-Endian.
+    
+    This is because the NIST Beacon Signature is generated using the Microsoft
+    CryptoAPI."""
+    return signature.decode("hex")[::-1]
+
+def verify(signature, data, cfile='./beacon.cer'):
+    """Check if the signature matches on the data.
+    
+    signature -- the reported signature value in the beacon
+    data -- the constructed data to be checked to match the signature
+    cfile -- the beacon public key certificate file (default: ./beacon.cer)"""
     try:
-        int(args[0])+1
-    except:
-        parser.error("Timestamp is not an integer.")
+        cert_file = open(cfile, 'r').read()
+    except IOError:
+        print "Can not read certification file %s." % cfile
         sys.exit(1)
-    if int(args[0]) < 1378395540 and options.verbose:
-        print "The first available beacon is at 1378395540, using that."
-    ts = args[0]
-
-# Load the correct public key using a non-automated method. Make sure it's 
-# read-only for extra paranoia.
-try:
-    cert_file = open(options.cfile, 'r').read()
-except:
-    print "Certificate file \"" + options.cfile + "\" could not be read, \
-you can get it from https://beacon.nist.gov/home"
-    sys.exit(1)
-beacon_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, \
+    beacon_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, \
     cert_file)
+    try:
+        OpenSSL.crypto.verify(beacon_cert, \
+        reverse_sign(signature), data, 'sha512')
+    except OpenSSL.crypto.Error:
+        return False
+    return True
 
-# Load the record from one-minute ago in a beacon-dict.
-record = requests.get(record_url + ts, verify=True)
-xml = ET.fromstring(record.content)
-beacon={}
-for child in xml:
-    beacon[child.tag] = child.text
+def check_output(signature, output):
+    """Test if the SHA512 hash of the signature matches the beacon output value.
+    
+    signature -- the signature value from the beacon
+    output -- the output value from the beacon"""
+    sign_hash = hashlib.sha512(signature.decode("hex")).hexdigest().upper()
+    if sign_hash != output:
+        return False
+    return True
 
-if options.verbose:
-    print "Timestamp: " + beacon['timeStamp']
-    print "Output Value: " + beacon['outputValue']
-
-# Compose the data to be signed.
-data = beacon['version'] + \
-    "{0:08x}".format(int(beacon['frequency'])).decode("hex") + \
-    "{0:016x}".format(int(beacon['timeStamp'])).decode("hex") + \
-    beacon['seedValue'].decode("hex") + \
-    beacon['previousOutputValue'].decode("hex") + \
-    "{0:08x}".format(int(beacon['statusCode'])).decode("hex")
-
-# Convert from LE to BE because the NIST Beacon signature is created on MSWin.
-rev_sign = beacon['signatureValue'].decode("hex")[::-1]
-
-# Check if the signature matches.
-try:
-    OpenSSL.crypto.verify(beacon_cert, rev_sign, data, 'sha512')
-except OpenSSL.crypto.Error:
-    print "Beacon signature verification failed."
-    sys.exit(1)
-if options.verbose:
-    print "Beacon signature verification passed."
-
-# Hash the provided, validated signature.
-sign_hash = hashlib.sha512(beacon['signatureValue'].decode("hex")).hexdigest().upper()
-
-if sign_hash != beacon['outputValue']:
-    print "Signature hash does not match Output Value."
-    sys.exit(1)
-if options.verbose:
-    print "Signature hash successfully matches Output Value."
+if __name__ == "__main__":
+    main()
